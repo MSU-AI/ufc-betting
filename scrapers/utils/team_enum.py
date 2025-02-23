f32c60c33ebdbe74b394186456be5d6cb0766ce0
sardinger <sardinger@bizstream.com>
2025-02-23 17:47:53 -0500

    update/insert scrape scripts, added py venv locally

---
+++ scrapers/utils/team_enum.py
@@ -0,0 +1,34 @@
+from enum import Enum
+
+
+class NBATeam(Enum):
+    ATL = "ATL"
+    BOS = "BOS"
+    BRK = "BRK"  # BKN
+    CHO = "CHO"  # CHA
+    CHI = "CHI"
+    CLE = "CLE"
+    DAL = "DAL"
+    DEN = "DEN"
+    DET = "DET"
+    GSW = "GSW"
+    HOU = "HOU"
+    IND = "IND"
+    LAC = "LAC"
+    LAL = "LAL"
+    MEM = "MEM"
+    MIA = "MIA"
+    MIL = "MIL"
+    MIN = "MIN"
+    NOP = "NOP"
+    NYK = "NYK"
+    OKC = "OKC"
+    ORL = "ORL"
+    PHI = "PHI"
+    PHO = "PHO"  # PHX
+    POR = "POR"
+    SAC = "SAC"
+    SAS = "SAS"
+    TOR = "TOR"
+    UTA = "UTA"
+    WAS = "WAS"
