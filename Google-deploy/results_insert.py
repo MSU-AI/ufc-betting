683ee1a270dc9d16c06f16d816414a5b94eb4b73
5Minty <ardingersam@gmail.com>
2025-04-04 00:44:37 -0400

    kelly refactoring, replaced generate_ev with run_inference_pipeline

--- Google-deploy/results_insert.py
+++ Google-deploy/results_insert.py
@@ -49,20 +49,7 @@ def get_team_code(full_team_name: str) -> str:
     return None
 
 
-def calculate_kelly(p_model: float, odds: int) -> float:
-    # Convert odds to decimal odds
-    decimal_odds = 0
-    if odds < 0:  # 'minus' odds
-        decimal_odds = (100 / -(odds)) + 1
-    else:
-        decimal_odds = (odds / 100) + 1
-
-    b = decimal_odds - 1
-    q = 1 - p_model
-    kelly = (b * p_model - q) / b if b != 0 else 0
-    return round(kelly, 4)  # Neg kelly means to take the other side
-
-
+# Restructure results to be inserted into db
 def insert_results(results: List[Dict]):
     collection = connect_to_mongodb()
     if collection is None:
@@ -95,9 +82,8 @@ def insert_results(results: List[Dict]):
             away_odds = bookmaker["odds"]["away"]
             home_ev = bookmaker["expected_value"]["home"]
             away_ev = bookmaker["expected_value"]["away"]
-
-            home_kelly = calculate_kelly(home_win_prob, home_odds)
-            away_kelly = calculate_kelly(away_win_prob, away_odds)
+            home_kelly = bookmaker["kelly_fractions"]["home_kelly"]
+            away_kelly = bookmaker["kelly_fractions"]["away_kelly"]
 
             game_result = {
                 "home_team": home_team,
