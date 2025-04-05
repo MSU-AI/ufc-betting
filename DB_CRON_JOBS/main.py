fe7c94e525e48609a87bc46b351e08d06eefeb04
DevT02 <devliveforever@gmail.com>
2025-04-05 00:20:17 -0400

    ensures function can connect to the job properly

--- DB_CRON_JOBS/main.py
+++ DB_CRON_JOBS/main.py
@@ -1,7 +1,24 @@
-import subprocess
+import google.auth
+import google.auth.transport.requests
+import requests
 
 def dbcronupdate(event, context):
-    # executes job
-    result = subprocess.run(['gcloud', 'beta', 'run', 'jobs', 'execute', 'db-cron-jobs-job', '--region', 'us-central1'], capture_output=True, text=True)
-    print("STDOUT:", result.stdout)
-    print("STDERR:", result.stderr)
+    project = "civil-cascade-454901-m2"  
+    region = "us-central1"
+    job_name = "db-cron-jobs-job"
+    
+    url = f"https://{region}-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/{project}/jobs/{job_name}:run"
+    
+    credentials, _ = google.auth.default()
+    auth_req = google.auth.transport.requests.Request()
+    credentials.refresh(auth_req)
+    token = credentials.token
+
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
+    
+    response = requests.post(url, headers=headers, json={})
+    print("Response Code:", response.status_code)
+    print("Response Text:", response.text)
